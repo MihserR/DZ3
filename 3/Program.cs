8b2b43316f2672a72db090850c1738c33f03ac9f
Mihser <mihser@bk.ru>
2023-01-20 23:07:58 +0300

    Добавил 2-й вариант решения задачи 3

--- 3/Program.cs
+++ 3/Program.cs
@@ -1,11 +1,22 @@
 ﻿// Написать программу вычисления произведения чисел от 1 до N
+// Console.WriteLine("Ведите число: ");
+// int n = int.Parse(Console.ReadLine()!);
+// int a = n;
+// int p = 1;
+// for(int i = 1; i <= n; i++)
+// {
+//     p *= i;
+// }
+
+// Console.WriteLine("Произведение чисел от 1 до "+ a +" = " + p);
+
 Console.WriteLine("Ведите число: ");
 int n = int.Parse(Console.ReadLine()!);
-int a = n;
-int p = 1;
-for(int i = 1; i <= n; i++)
+int count = 1;
+int itog = 1;
+while (count <= n)
 {
-    p *= i;
+    itog = itog * count;
+    count++;
 }
-
-Console.WriteLine("Произведение чисел от 1 до "+ a +" = " + p);
+Console.WriteLine(itog);
