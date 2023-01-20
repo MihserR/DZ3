154bd6f9744ecb25078c2e0ace38f80b18b341a6
Mihser <mihser@bk.ru>
2023-01-20 22:47:50 +0300

    add folder 3

---
+++ 3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+Console.WriteLine("Ведите число: ");
+int n = int.Parse(Console.ReadLine()!);
+int a = n;
+int p = 1;
+for(int i = 1; i <= n; i++)
+{
+    p *= i;
+}
+
+Console.WriteLine("Произведение чисел от 1 до "+ a +" = " + p);
