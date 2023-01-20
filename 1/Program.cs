9a2eb66040235a9896bd92df10e31540bf6e0540
Mihser <mihser@bk.ru>
2023-01-20 22:08:25 +0300

    add folder 1

---
+++ 1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Найти кубы чисел от 1 до N
+Console.WriteLine("Введите число: ");
+
+int N = int.Parse(Console.ReadLine()!);
+int number = 1;
+
+while (number <= N)
+{
+    Console.WriteLine(number * number * number);
+    number ++;
+}
