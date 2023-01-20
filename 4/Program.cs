16ed94abec0a1314a5a786c8a7c26a8e5e6f1a6d
Mihser <mihser@bk.ru>
2023-01-20 23:25:40 +0300

    add folder 4

---
+++ 4/Program.cs
@@ -0,0 +1,11 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+Console.WriteLine("Ведите число: ");
+int n = int.Parse(Console.ReadLine()!);
+int i = 1;
+
+while (i <= n)
+{
+    int res = i * i * i;
+    if (res % 2 == 0) System.Console.Write(res + " ");
+    i++;
+}
