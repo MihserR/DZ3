6ba0e93d894572846ef11076c4204812b98ba145
Mihser <mihser@bk.ru>
2023-01-20 22:30:26 +0300

    add folder 2

---
+++ 2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Подсчитать сумму цифр в числе
+Console.WriteLine("Ведите число");
+int n = int.Parse(Console.ReadLine()!);
+int a = n;
+int sum = 0;
+
+while (n > 0)
+{
+    sum = sum + n % 10;
+    n = n / 10;
+}
+Console.Write("Сумма цифр числа " + a + " = " + sum);
